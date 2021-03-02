c7c3258dbc4ef2d46d7870d157b6f01b4d735a07
Antonio Manuel Perez Lopez <aperezl@gmail.com>
2021-03-02 20:55:52 +0100

    first example

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,7 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+
+import logo from './logo.svg'
 
 function App() {
+  const nombre = 'Test'
+  const auth = true
+  const estaciones = ['Primeravera', 'Verano', 'Otoño', 'Invierno']
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +13,15 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1>{nombre}</h1>
+        <p> { auth ? 'user is validated' : 'user is not validated' }</p>
+        <p>{ 1 + 2 } </p>
+        <ul>
+          {estaciones.map((el, index) => (
+            <li key={index}>{el}</li>  
+          ))}
+        </ul>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
